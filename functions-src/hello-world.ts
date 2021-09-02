f0de03209304a5da372d899dab754f371c8d3481
Catalin Negru <catalinalexandrunegru@gmail.com>
2021-09-02 11:51:04 -0400

    added typescript to functions

--- functions-src/hello-world.ts
+++ functions-src/hello-world.ts
@@ -1,30 +1,33 @@
 // Imports global types
-import '@twilio-labs/serverless-runtime-types';
+import "@twilio-labs/serverless-runtime-types";
 // Fetches specific types
 import {
   Context,
   ServerlessCallback,
   ServerlessFunctionSignature,
-} from '@twilio-labs/serverless-runtime-types/types';
+} from "@twilio-labs/serverless-runtime-types/types";
 
 type MyEvent = {
-  Body?: string
-}
+  Body?: string;
+};
 
 // If you want to use environment variables, you will need to type them like
 // this and add them to the Context in the function signature as
 // Context<MyContext> as you see below.
 type MyContext = {
-  GREETING?: string
-}
+  GREETING?: string;
+};
+const serverless = async () => {
+  const message = {
+    status: "Hello World",
+  };
+  return message;
+};
 
-export const handler: ServerlessFunctionSignature = function(
+export const handler: ServerlessFunctionSignature = (
   context: Context<MyContext>,
   event: MyEvent,
   callback: ServerlessCallback
-) {
-  const message = {
-    status: 'Hello World'
-  }
+) => {
   callback(null, message);
 };
